db365fc1f0564d1bc8fd9a8202667438af2f827e
Samuel Loury <konubinixweb@gmail.com>
2022-12-14 17:21:16 +0100

    chore: Make explicit the fact that the dockerize command has a timeout

--- wait-for-vault.sh
+++ wait-for-vault.sh
@@ -5,7 +5,7 @@ set -x
 VAULT_ADDRESS=localhost:8200
 # unlike VAULT_ADDR, that contains the http:// prefix
 
-dockerize -wait "tcp://${VAULT_ADDRESS}"
+dockerize -timeout 10s -wait "tcp://${VAULT_ADDRESS}"
 
 echo "Waiting for vault to be initialized"
 while ! { wget -O - "http://${VAULT_ADDRESS}/v1/sys/health" 2>& 1 |grep -q '"initialized":true' ; }
