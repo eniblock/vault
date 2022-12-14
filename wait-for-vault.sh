eb67fcc5df42c5aa970ad82ab80d185af41d34b2
Samuel Loury <konubinixweb@gmail.com>
2022-12-14 17:21:44 +0100

    feat: Don't let the script run infinitely

--- wait-for-vault.sh
+++ wait-for-vault.sh
@@ -8,8 +8,17 @@ VAULT_ADDRESS=localhost:8200
 dockerize -timeout 10s -wait "tcp://${VAULT_ADDRESS}"
 
 echo "Waiting for vault to be initialized"
+max_time=10
+sleep_time=1
+waited_time=0
 while ! { wget -O - "http://${VAULT_ADDRESS}/v1/sys/health" 2>& 1 |grep -q '"initialized":true' ; }
 do
-    sleep 1
+    if test ${waited_time} -gt ${max_time}
+    then
+        echo "I already waited ${waited_time}. I won't wait any longer."
+        exit 1
+    fi
+    sleep ${sleep_time}
+    waited_time=$(( waited_time + sleep_time ))
 done
 echo "Vault ready to accept connections"
