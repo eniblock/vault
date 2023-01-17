6df3af67ba8bf0ed323371db328e67a65f9be497
Samuel Loury <konubinixweb@gmail.com>
2023-01-17 15:58:20 +0100

    feat: Register the plugin only when needed
    Either the plugin was not registered, or it has changed.

--- reload-plugin.sh
+++ reload-plugin.sh
@@ -18,11 +18,24 @@ export VAULT_TOKEN="${VAULT_ROOT_TOKEN}"
 
 HASH=$(sha256sum /vault/plugins/"${plugin_name}" | cut -f1 -d' ')
 
-vault plugin register -sha256 "${HASH}" "${plugin_name}"
-vault plugin reload -plugin "${plugin_name}"
-if vault secrets list | grep -q "^${plugin_name}/"
-then
-  vault secrets disable "${plugin_name}"
-fi
+vault_tss_already_registered () {
+    vault list sys/plugins/catalog/secret | grep -q "^${plugin_name}$"
+}
 
-vault secrets enable -path "${plugin_name}" -description "From plugin '${plugin_name}'" "$@" "${plugin_name}"
+vault_tss_registered_hash () {
+    vault plugin info -field sha256 secret "${plugin_name}"
+}
+
+if vault_tss_already_registered && test "$(vault_tss_registered_hash)" = "${HASH}"
+then
+    echo "Vault plugin ${plugin_name} already registered and up-to-date" >&2
+else
+    vault plugin register -sha256 "${HASH}" "${plugin_name}"
+    vault plugin reload -plugin "${plugin_name}"
+    if vault secrets list | grep -q "^${plugin_name}/"
+    then
+        vault secrets disable "${plugin_name}"
+    fi
+
+    vault secrets enable -path "${plugin_name}" -description "From plugin '${plugin_name}'" "$@" "${plugin_name}"
+fi
